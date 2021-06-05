0b61dc1613e407687f419d53540ec277cb8a20ea
heejungshim <hjshim@gmail.com>
2021-06-06 01:08:08 +0900

    add two more data sets

--- R/documentation.R
+++ R/documentation.R
@@ -1,22 +1,56 @@
 #' @useDynLib multiseq
-#' @title OAS1 gene data set containing 1) RNA-seq counts for each sample over  OAS1 gene region, 2) genotypes at a particular SNP, and 3)  metadata.
+#' @title OAS1 gene data set 
 #' @description This is a subset of the data collected in an RNA-Seq experiment on
 #' lymphoblastoid cell lines derived from unrelated Nigerian individuals (Pickrell et al.,
 #' Nature 464, 768-772, 2010). Specifically, it consists of a matrix of counts for 66
-#' samples over a region of length 2^12 (chr12:113354417-113358512) where SNP rs10774671
-#' was found to be associated with a differential expression pattern. Reads were
+#' samples over a region of length 2^12 (gene OAS1; chr12:113354417-113358512) where SNP rs10774671
+#' was found to be associated with splicing (see Figure 3 in Pickrell et al.). Reads were
 #' downloaded from http://eqtl.uchicago.edu/Home.html amd mapped to \code{hg19} with bwa.
-#' Covariates (genotypes) are available from the International HapMap Project.
+#' Genotypes are available from the International HapMap Project.
 #' @docType data
 #' @keywords datasets
 #' @format A list with elements: \code{x} a 66 by 2^12 matrix of counts, \code{read.depth}
 #' a 66-dimensional vector specifiying the total number of counts per sample, a string \code{SNP}
-#' specifying the SNP name, \code{g} a 66-dimensional vector specifying the genotype (covariate)
-#' for each sample, \code{assembly} specifying the genome that reads were mapped to,
+#' specifying the SNP name, \code{g} a 66-dimensional vector specifying the genotype for each sample, 
+#' \code{assembly} specifying the genome that reads were mapped to,
 #' \code{region} a string specifying the region of length 2^12 that reads were mapped to.
 #' @name OAS1
 NULL
 
+
+#' @title DNase seq data over chr17:10160989-10162012  
+#' @description This is a subset of the data collected in a DNase-seq experiment on
+#' lymphoblastoid cell lines derived from unrelated Nigerian individuals (Degner et al., Nature
+#' 482, 390–4, 2012). Specifically, it consists of a matrix of counts for 70
+#' samples over a region of length 2^10 (chr17:10160989-10162012) where SNP chr17.10161485 
+#' was found to be associated with chromatin accessibility (see Figure 2 in Shim and Stephens, 
+#' Ann. Appl. Stat. 9 665–686, 2015). DNase-seq reads and genotype data were downloaded from 
+#' http://eqtl.uchicago.edu/dsQTL_data/.
+#' @docType data
+#' @keywords datasets
+#' @format A list with elements: \code{x} a 70 by 2^10 matrix of counts, \code{read.depth}
+#' a 70-dimensional vector specifiying the total number of counts per sample, a string \code{SNP}
+#' specifying the SNP name, \code{g} a 70-dimensional vector specifying the genotype at the SNP for each sample, 
+#' \code{region} a string specifying the region of length 2^10 that reads were mapped to.
+#' @name chr17.10160989.10162012.DNase.seq
+NULL
+
+#' @title ATAC-seq data over chr1:111764939-111765962
+#' @description This is a subset of the data collected in an ATAC-seq experiment on 3 copper treated 
+#' samples and 3 control samples (Shim et al. 2021). Specifically, it consists of a matrix of counts 
+#' for 6 samples over a region of length 2^10 (chr1:111764939-111765962) where chromatin accessibility 
+#' is differentially expressed between two conditions (see Figure 3 in Shim et al. 2021). 
+#' @docType data
+#' @keywords datasets
+#' @format A list with elements: \code{x} a 6 by 2^10 matrix of counts, \code{read.depth}
+#' a 6-dimensional vector specifiying the total number of counts per sample, \code{g} a 
+#' 6-dimensional vector specifying the group of each sample (copper treated samples vs control samples),
+#' \code{region} a string specifying the region of length 2^10 that reads were mapped to, \code{overall.logLR} 
+#' a number indicating log likelihood ratio for overall expression obtained from DESeq2 output, 
+#' \code{overall.effect} a number indicating the effect size for overall expression obtained from DESeq2 output, 
+#' \code{overall.effect.var} a number indicating the variance of the effect size obtained from DESeq2 output.
+#' @name chr1.111764939.111765962.ATACseq
+NULL
 
 #' multiseq: multi-scale Poisson process approaches for differential expression analysis or association analysis of high-throughput sequencing data
 #' @docType package
